b6961d551ec937023ac0bcf3a5ce45d2493f5917
niteshTalentpod <nitesh.rajak@talentpod.com>
2019-06-17 18:18:57 +0530

    test

---
+++ click4jobs/src/app/post.ts
@@ -0,0 +1,17 @@
+export class Post {
+    id: number;
+    name: string;
+    description: string;
+    company: string;
+    domain: string;
+    skillset: string;
+    salary: number;
+    maxSalary: number;
+    recruiter: string;
+    postedDate: string;
+    expiryDate: string;
+    applicantList: number;
+    experiance: number;
+    active: string;
+    location: string;
+}
